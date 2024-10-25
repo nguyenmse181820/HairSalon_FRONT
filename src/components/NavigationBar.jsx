65dfbbbdf965cdb209e21c9686eb205cb746d879
Nguyen <nguyenmse181820@fpt.edu.vn>
2024-10-25 16:38:55 +0700

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Logo from '../assets/coiffure-logo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faHeadphonesSimple, faMagnifyingGlass, faUser, faCalendar, faBell, faTimes } from '@fortawesome/free-solid-svg-icons';
