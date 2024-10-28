3ee0497f446b9bbbb662092858ea44a155d60398
ngynphong <124499195+ngynphong@users.noreply.github.com>
2024-10-28 17:05:28 +0700

    m

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import CustomerFrame from './pages/frame/CustomerFrame.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import AccountPage from './pages/AccountPage.jsx';
+
 import './index.css'
 import StylistFrame from './pages/frame/StylistFrame.jsx';
 import StylistPage from './pages/stylistpage/StylistPage.jsx';
@@ -11,46 +11,60 @@ import ServiceStatus from './pages/stylistpage/ServiceStatus.jsx';
 import AppointmentView from './pages/stylistpage/AppointmentView.jsx';
 import ManagementAppointment from './pages/stylistpage/ManagementAppointment.jsx';
 import Earning from './pages/stylistpage/Earning.jsx';
+import ScheduleManagement from './pages/stylistpage/ScheduleManagement.jsx';
 
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <CustomerFrame />,
-      children: [
-        { path: "", element: <LandingPage /> },
-        {
-          path: "account",
-          element: user?.isLoggedIn ? <ProfilePage /> : <AccountPage />,
-        },
-        { path: "about-us", element: <AboutUs /> },
-        { path: "contact", element: <ContactPage /> },
-      ],
-    },
-    {
-      path: "/stylist",
-      element: <StylistFrame />,
-      children: [
-        { path: "home", element: <StylistPage /> },
-        { path: "services", element: <ServiceStatus /> },
-        { path: "appointmentView", element: <AppointmentView /> },
-        { path: "appointmentManagement", element: <ManagementAppointment /> },
-        { path: "earning", element: <Earning /> },
-      ],
-    },
-  ]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CustomerFrame />,
+    children: [
+      {
+        path: '',
+        element: <LandingPage />
+      },
+      {
+        path: 'account',
+        element: <AccountPage />
+      },
+      
 
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <RouterProvider router={router} />
-    </UserContext.Provider>
-  );
-};
-const container = document.getElementById("root");
-const root = createRoot(container);
+    ]
+  },
+  {
+    path: '/stylist',
+    element: <StylistFrame />,
+    children: [
+      {
+        path: 'home',
+        element: <StylistPage />
+      },
+      {
+        path: 'services',
+        element: <ServiceStatus />
+      },
+      {
+        path: 'appointmentView',
+        element: <AppointmentView />
+      },
+      {
+        path: 'appointmentManagement',
+        element: <ManagementAppointment />
+      },
+      {
+        path: 'earning',
+        element: <Earning />
+      },
+      {
+        path: 'schedule',
+        element: <ScheduleManagement />
+      }
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+    ]
+  },
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
