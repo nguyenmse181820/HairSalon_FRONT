e4cfb2b685f0ca27e14932279dd80b1473c86cd7
theweakant <nam503888@gmail.com>
2024-11-07 12:45:44 +0700

    fix UI desgin staff

--- src/pages/stylistpage/AppointmentView.jsx
+++ src/pages/stylistpage/AppointmentView.jsx
@@ -1,55 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function AppointmentView() {
-    
     const [appointments, setAppointments] = useState([]);
+
     useEffect(() => {
-        axios.get('https://6721ddfa98bbb4d93caa0c5e.mockapi.io/appointment').then((res) => {
-            setAppointments(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })       
-    })
+        axios.get('https://6721ddfa98bbb4d93caa0c5e.mockapi.io/appointment')
+            .then((res) => setAppointments(res.data))
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
-        <div>
-            <div className='mt-10 font-bold text-lg md:text-xl text-center uppercase tracking-wider'>Appointment</div>
-            <div className='shadow-lg flex flex-col mt-10 mb-10 mx-6 lg:mx-10 text-sm lg:text-lg'>
-                <table className='table-auto border border-solid border-l-0 border-r-0'>
-                    <thead className=''>
-                        <tr>
-                            <th>Stt</th>
-                            <th>Customer Name</th>
-                            <th className='hidden sm:table-cell'>Service Type</th>
-                            <th>Date</th>
-                            <th className='hidden sm:table-cell'>Time</th>
-                            <th>Status</th>
+        <div className="bg-white rounded-lg p-6 shadow-lg">
+            <h4 className="text-2xl font-bold text-black mb-10 uppercase text-center">Appointment</h4>
+
+            <table className="w-full text-left mt-1">
+                <thead>
+                    <tr className="text-lg font-semibold text-black">
+                        <th className="py-2 px-3 font-semibold text-center uppercase">Stt</th>
+                        <th className="py-2 px-3 font-semibold text-center uppercase">Customer Name</th>
+                        <th className="py-2 px-3 font-semibold hidden sm:table-cell text-center uppercase">Service Type</th>
+                        <th className="py-2 px-3 font-semibold text-center uppercase">Date</th>
+                        <th className="py-2 px-3 font-semibold hidden sm:table-cell text-center uppercase">Time</th>
+                        <th className="py-2 px-3 font-semibold text-center uppercase">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colSpan="6">
+                            <hr className="w-full border-gray-300 my-2" />
+                        </td>
+                    </tr>
+                    {appointments.map((appointment) => (
+                        <tr key={appointment.id}>
+                            <td className="p-2 text-center">{appointment.id}</td>
+                            <td className="p-2 text-center">{appointment.customerName}</td>
+                            <td className="p-2 hidden sm:table-cell text-center">{appointment.serviceType}</td>
+                            <td className="p-2 text-center">{appointment.date}</td>
+                            <td className="p-2 hidden sm:table-cell text-center">{appointment.time}</td>
+                            <td
+                                className={`p-2 text-center uppercase ${appointment.status === 'Success' ? 'text-green-500' :
+                                    appointment.status === 'Canceled' ? 'text-red-500' :
+                                        appointment.status === 'Waiting' ? 'text-yellow-500' : ''
+                                    }`}
+                            >
+                                {appointment.status}
+                            </td>
                         </tr>
-
-                    </thead>
-                    <tbody className='text-center  '>
-                        {appointments.map((appointment) => {
-                            return (
-                                <tr key={appointment.id} className=''>
-                                    <td className='py-4'>{appointment.id}</td>
-                                    <td className='py-4'>{appointment.customerName}</td>
-                                    <td className='hidden sm:table-cell py-4'>{appointment.serviceType}</td>
-                                    <td className='py-4'>{appointment.date}</td>
-                                    <td className='hidden sm:table-cell py-4'>{appointment.time}</td>
-                                    <td className='flex justify-center py-4'>
-                                        <div className=''>{appointment.status}</div>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                        }
-
-                    </tbody>
-                </table>
-            </div>
-
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
-export default AppointmentView
+export default AppointmentView;
